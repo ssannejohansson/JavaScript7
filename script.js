5496fb31a36db3c6a10eda85c0e7e327cc804f1a
ssannejohansson <sannej0hansson@hotmail.com>
2025-04-10 19:05:24 +0200

    javascript added and working quite well
    everything except the addWordToDOM is working

--- script.js
+++ script.js
@@ -33,10 +33,56 @@ const words = [
 ];
 
 //Initializing word
-let randomWord;
+let randomWord = words[Math.floor(Math.random()*words.length)]; 
+word.innerText = randomWord;
 
 //Initializing score
 let score = 0;
 
 //Initializing time
 let time = 10;
+
+
+
+
+function addWordToDOM () {
+word.innerText = words[Math.floor(Math.random()*words.length)]
+}
+
+
+
+function updateScore () {
+++score; 
+scoreEl.innerText = score; 
+}
+
+let timeout = setInterval(updateTime, 1000);
+
+function updateTime () {
+  time--;
+  timeEl.innerText = time;
+    if (time < 0) {
+      clearInterval (timeout)
+      gameOver(); 
+    }
+  }
+
+  function gameOver () {
+  document.getElementById("end-game-container").style.display = "block"; 
+  }
+
+
+text.addEventListener("keypress", function () {
+  if (this.value == randomWord) {
+    // call updateScore
+    updateScore(); 
+    // give the user a new word by calling addWordToDom
+    addWordToDOM();
+    // increment time by 5 seconds
+    timeEl.innerText = time += 5; 
+    // reset input to empty string
+    let newWord = this.value; 
+    this.value = ""; 
+  }
+});
+
